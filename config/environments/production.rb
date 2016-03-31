8cf1742a747116a644e91bfa698aefdfe6d77c0b
bjsmth <bj7smth@gmail.com>
2016-03-31 19:25:57 +1000

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,11 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  # Action Cable endpoint configuration
+  config.action_cable.allowed_request_origins = [ 'http://useli.ru', /http:\/\/udeli.*/ ]
+  # config.action_cable.url = '/cable'
+  config.force_ssl = false
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
